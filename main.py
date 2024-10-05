71d35d0c77097f1ecc41d8b28259e4df8e7a5209
aathikm <aathikm@hotmail.com>
2024-10-05 13:36:05 +0530

    updated the host

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ async def scrap_and_send_column(request: ScrapRequest):
     return {"news": column_data}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
